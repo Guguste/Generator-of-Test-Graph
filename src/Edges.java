61be599ebe7c00cb1df85d6041172dc38eab3e3e
Kiwhacks <pierre.limballe@hotmail.fr>
2014-08-08 17:46:42 +0200

    Addition of a menu

---
+++ src/Edges.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+import edu.umd.cs.piccolo.PLayer;
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PPath;
+
+
+public class Edges {
+
+        private PPath edge;
+
+        public Edges(PNode node1, PNode node2){
+                edge = new PPath();
+                edge.addAttribute("nodes", new ArrayList());
+        ((ArrayList<PPath>)node1.getAttribute("edges")).add(edge);
+        ((ArrayList<PPath>)node2.getAttribute("edges")).add(edge);
+        ((ArrayList<PNode>)edge.getAttribute("nodes")).add(node1);
+        ((ArrayList<PNode>)edge.getAttribute("nodes")).add(node2);
+        }
+        public Edges(PPath edge){
+                this.edge = edge;
+
+        }
+
+        public PPath getEdge(){
+                return edge;
+        }
+        public void deleteEdge(PLayer edgeLayer){
+                edgeLayer.removeChild((PNode)edge);
+        }
+}
